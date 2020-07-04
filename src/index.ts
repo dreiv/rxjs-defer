cada0357e6a9371aeafeca16f8a3cae0ba133879
Andrei Voicu <dreivoicu@gmail.com>
2020-07-05 00:19:48 +0300

    used defer

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import { interval } from 'rxjs';
 
 import { tapOnce } from './tapOnce'
 
-const source = interval(5000).pipe(
-  tapOnce(() => console.log('🦊')
+const source = interval(1000).pipe(
+  tapOnce(() => console.log('Fox')
 ));
 
+source.subscribe(console.log);
 source.subscribe(console.log);
--- src/tapOnce.ts
+++ src/tapOnce.ts
@@ -1,16 +1,19 @@
-import { Observable, OperatorFunction } from 'rxjs';
+import { Observable, OperatorFunction, defer } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 export function tapOnce<T>(fn: Function): OperatorFunction<T, T> {
-  return function (source: Observable<any>) {
-    let run = false;
-    return source.pipe(
-      tap(() => {
-        if (!run) {
-          fn();
-          run = true;
-        }
-      })
-    );
-  };
+  return (source: Observable<any>) => {
+    return defer(() => {
+      let run = false;
+
+      return source.pipe(
+        tap(() => {
+          if (!run) {
+            fn();
+            run = true;
+          }
+        })
+      );
+    })
+  }
 }
